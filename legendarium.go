becb8c57b5b6b7da37c9fc648ea43111cec50705
Rafael Stauffer <rafael.stauffer@hftm.ch>
2023-11-05 15:35:50 +0100

    feat: read all jsons from a folder

--- legendarium.go
+++ legendarium.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -11,8 +14,18 @@ import (
 type LegendCharacter struct {
 	gorm.Model
 
+	Name        string `gorm:"uniqueIndex"`
+	Description string
+	Milestones  []LegendMilestone
+}
+
+// Contains Character sheet and metadata valid in a point of time
+type LegendMilestone struct {
+	gorm.Model
+	LegendCharacterID uint // foreign key for the has many reference
+
+	ReferenceDate  time.Time
 	CharacterSheet string
-	Name           string
 	Description    string
 }
 
@@ -26,6 +39,91 @@ func readCharsheet(path string) (string, error) {
 	return string(content), nil
 }
 
+// extracted info by getPaths
+type PathInfo struct {
+	Name    string
+	Path    string
+	RefDate time.Time
+	Note    string
+}
+
+func getPaths(basepath string) []PathInfo {
+	dirs, err := os.ReadDir(basepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	paths := make([]PathInfo, 0)
+
+	for _, dir := range dirs {
+		if !dir.IsDir() {
+			if !strings.Contains(dir.Name(), "_") || !strings.Contains(dir.Name(), ".json") {
+				continue
+			}
+
+			fullPath := filepath.Join(basepath, dir.Name())
+
+			substrings := strings.Split(strings.Split(dir.Name(), ".")[0], "_")
+
+			dateOnlyString := substrings[1][4:] + "-" + substrings[1][2:4] + "-" + substrings[1][0:2] // "2006-01-02"
+
+			refDate, err := time.Parse(time.DateOnly, dateOnlyString)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			notes := strings.Join(substrings[2:], " ")
+
+			paths = append(paths, PathInfo{
+				Name:    substrings[0],
+				Path:    fullPath,
+				RefDate: refDate,
+				Note:    notes,
+			})
+		}
+	}
+
+	return paths
+}
+
+func loadFolder() []*LegendCharacter {
+
+	legendChars := make([]*LegendCharacter, 0)
+
+	for i, pInfo := range getPaths("../") {
+		log.Printf("%v %v", i, pInfo)
+
+		var currentLegendChar *LegendCharacter
+		currentLegendChar = nil
+		for _, lc := range legendChars {
+			if lc.Name == pInfo.Name {
+				currentLegendChar = lc
+			}
+		}
+		if currentLegendChar == nil {
+			currentLegendChar = &LegendCharacter{
+				Name: pInfo.Name,
+			}
+			legendChars = append(legendChars, currentLegendChar)
+		}
+
+		cs, err := readCharsheet(pInfo.Path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		newMilestone := &LegendMilestone{
+			CharacterSheet: cs,
+			ReferenceDate:  pInfo.RefDate,
+			Description:    pInfo.Note,
+		}
+
+		currentLegendChar.Milestones = append(currentLegendChar.Milestones, *newMilestone)
+	}
+
+	return legendChars
+}
+
 func main() {
 	db, err := gorm.Open(sqlite.Open("legends.db"), &gorm.Config{})
 	if err != nil {
@@ -33,15 +131,10 @@ func main() {
 	}
 
 	db.AutoMigrate(&LegendCharacter{})
+	db.AutoMigrate(&LegendMilestone{})
 
-	cs, err := readCharsheet("../Far'dei.json")
-	if err != nil {
-		log.Fatal(err)
+	for _, legend := range loadFolder() {
+		log.Print(legend.Name)
+		db.Save(legend)
 	}
-
-	newLegendChar := &LegendCharacter{
-		CharacterSheet: cs,
-	}
-
-	db.Create(newLegendChar)
 }
